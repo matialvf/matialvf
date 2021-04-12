627e7832b07e160d6bbbd53e79cf7a325c4881ec
Matias Alvarez <Mati@Matiass-MacBook-Pro.local>
2021-04-12 19:25:37 -0300

    working with fermin on css changes

---
+++ application/routes.py
@@ -0,0 +1,18 @@
+from flask import render_template
+from application import app
+
+@app.route('/', methods=['GET'])
+@app.route('/index', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/list', methods=['GET'])
+def list():
+    return render_template('list.html')
+
+@app.route('/add-item', methods=['POST'])
+def add_item():
+	return '<h1>item added succesfully</h1>'
+
+# CRUD operations 
+# http type of requests and most common return codes
