771d171e3200f91f5fce400190053cfbb20f7aa6
Matias Alvarez <Mati@Matiass-MacBook-Pro.local>
2021-04-20 17:03:07 -0300

    added login.html + @app.route

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from application import app
+from application.forms import LoginForm
 
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
@@ -10,6 +11,14 @@ def index():
 def list():
     return render_template('list.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		flash('Login requested for user {}. remember_me={}'.format(form.username.data, form.remember_me.data))
+		return redirect('/index')
+	return render_template('login.html', title='Sign In', form=form)
+
 @app.route('/add-item', methods=['POST'])
 def add_item():
 	return '<h1>item added succesfully</h1>'
@@ -17,6 +26,7 @@ def add_item():
 @app.route('/delete-item/<id>', methods=['DELETE'])
 def delete_item(id):
 	pass
+
 
 
 # CRUD operations 
