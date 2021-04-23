c1827b5dc578c74ade16987021075e87cb2f2c33
Matias Alvarez <Mati@Matiass-MacBook-Pro.local>
2021-04-22 22:55:40 -0300

    Agregue Signup.html, Route Signup POST, en forms SignupForm. Update styles.css.

--- application/forms.py
+++ application/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, Form, validators
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
@@ -8,6 +8,13 @@ class LoginForm(FlaskForm):
 	remember_me = BooleanField('Remember Me')
 	submit = SubmitField('Sign In')
 
-class AddItem(FlaskForm):
-    # time =...
-    pass
+class SignupForm(Form):
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email Address', [validators.Length(min=6, max=35)])
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+    accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
+    submit = SubmitField('Sign Up')
--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, request
 from application import app, db
-from application.forms import LoginForm
+from application.forms import LoginForm, SignupForm
 from application.models import Item
 from datetime import datetime
 
@@ -14,6 +14,16 @@ def list():
     items = Item.query.all()
     return render_template('list.html', items=items)
 
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignupForm(request.form)
+    if request.method == 'POST' and form.validate_on_submit():
+        user = User(form.username.data, form.email.data, form.password.data)
+        db_session.add(user)
+        flash('Thanks for registering')
+        return redirect(url_for('login'))
+    return render_template('signup.html', title='Sign Up', form=form)
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 	form = LoginForm()
@@ -24,8 +34,8 @@ def login():
 
 @app.route('/add-item', methods=['POST'])
 def add_item():
-    # submitted_time = request.form.get('date') + ' ' + request.form.get('time')
-    # dt = datetime.strptime(submitted_time, '%Y-%m-%d H:i:s')
+    submitted_time = request.form.get('date') + ' ' + request.form.get('time')
+    dt = datetime.strptime(submitted_time, '%Y-%m-%d H:i:s')
     dt = datetime.strptime(request.form.get('time'), '%Y-%m-%d')
     item = Item(time=dt, event=request.form.get("event"), description=request.form.get("description"))
     db.session.add(item)
